53e47c7de2a3a52776a3c53701f9c1711827f528
Syafiq <isyafiq@gmail.com>
2024-03-05 16:43:27 +0800

    created diff navbar for mobile and desktop

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import DesktopNavbar from './components/DesktopNavbar.vue'
+import MobileNavbar from './components/MobileNavbar.vue'
+
+ function isMobile() {
+   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+     return true
+   } else {
+     return false
+   }
+ }
+
 </script>
 
 <template>
-  <nav class="*:transition-colors duration-75 flex justify-end sticky top-0 bg-zinc-950 *:w-40 text-center lg:bg-inherit">
-    <RouterLink class=" py-6 hover:bg-white hover:bg-opacity-5" to="/">home</RouterLink>
-    <RouterLink class=" py-6 hover:bg-white hover:bg-opacity-5" to="/about">about</RouterLink>
-    <RouterLink class=" py-6 hover:bg-white hover:bg-opacity-5" to="/contact">contact</RouterLink>
-  </nav>
 
+  <DesktopNavbar v-if="!isMobile()"/>
+  <MobileNavbar v-else />
+  
   <div class="m-8 lg:my-16 lg:mx-64">
     <RouterView/>
   </div>
---
+++ src/components/MobileNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="sticky top-0 shadow-md bg-black" ref="navbar">
+        <div class="p-6 w-fit transition scale-150" :class="{'scale-90': navbarVisible}" @click="toggleNavbar()">
+            <BurgerMenuIcon />
+        </div>
+        <div class="flex flex-col transition absolute backdrop-blur-lg h-dvh" :class="{'-translate-x-full': !navbarVisible}" >
+            <RouterLink @click.native="toggleNavbar()" class="p-6 w-[50vw] hover:bg-white hover:bg-opacity-5" to="/">home</RouterLink>
+            <RouterLink @click.native="toggleNavbar()" class="p-6 w-[50vw] hover:bg-white hover:bg-opacity-5" to="/about">about</RouterLink>
+            <RouterLink @click.native="toggleNavbar()" class="p-6 w-[50vw] hover:bg-white hover:bg-opacity-5" to="/contact">contact</RouterLink>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import { onClickOutside } from '@vueuse/core'
+import BurgerMenuIcon from '../assets/icons/BurgerMenuIcon.vue'
+
+const navbarVisible = ref(false)
+function toggleNavbar() {
+    navbarVisible.value = !navbarVisible.value
+}
+
+const navbar = ref(null)
+onClickOutside(navbar, () => hideNavbar())
+</script>
