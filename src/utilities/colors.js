0d1433f39b326222713f4862fbbac4141e8fc589
Syafiq <isyafiq@gmail.com>
2024-03-06 15:20:30 +0800

    update colors

--- src/utilities/colors.js
+++ src/utilities/colors.js
@@ -1,12 +1,9 @@
 export const colors = [
     'text-teal-500',
-    'text-green-500',
     'text-cyan-500',
     'text-sky-500',
     'text-lime-500',
     'text-emerald-500',
-    'text-fuchsia-500',
-    'text-pink-500',
     'white'
   ]
   
