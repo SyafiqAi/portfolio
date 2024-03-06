a5d7e639a0cf8e5ebbdf7b8eb0825f51b93d549b
Syafiq <isyafiq@gmail.com>
2024-03-06 15:19:30 +0800

    update introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,11 +1,14 @@
 <template>
-    <section class="lg:max-w-96 *:pb-6">
-        <p>
-            I'm a final year student at University Tunku Abdul Rahman ready to put my coding skills to the test.
-        </p>
-        <p>
-            My internship at <strong>Digital Systems Malaysia</strong> has allowed me to apply my skills
-            in a professional setting and further develop my interest in web development. 
-        </p>
+    <section>
+        <h2>Introduction</h2>
+        <div class="*:pb-6 lg:max-w-[40vw]">
+            <p>
+                I'm a final year student at University Tunku Abdul Rahman.
+            </p>
+            <p>
+                My internship at <strong>Digital Systems Malaysia</strong> has allowed me to apply my skills
+                in a professional setting and further develop my interest in web development. 
+            </p>
+        </div>
     </section>
 </template>
