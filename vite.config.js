c58680280372d0cdfe5b8db13338c03d0538b8bc
Syafiq <isyafiq@gmail.com>
2024-03-06 09:42:44 +0800

    inkscape svg error fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['sodipodi:namedview'].includes(tag), // to avoid errors when using inkscape svg
+        }
+      }
+    })
   ],
   resolve: {
     alias: {
