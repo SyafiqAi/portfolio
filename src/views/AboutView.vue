035963d0a9294fcf6a049af8ff2b231054526c31
Syafiq <isyafiq@gmail.com>
2024-03-09 21:41:56 +0800

    changed icon

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="about">
-    <h1 class="">This is an about page</h1>
+    <h1 class="text-9xl">About</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
