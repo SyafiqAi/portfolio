cee187df4b72fddaba113c2173320b81f0947014
Syafiq <isyafiq@gmail.com>
2024-04-02 10:35:06 +0800

    contact page styles

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="lg:h-screen justify-center my-[30vw] lg:items-center lg:justify-normal lg:mx-32 lg:my-0 lg:grid lg:grid-cols-2 lg:py-[72px]"
+    class="lg:h-screen h-[100lvh] -mt-[79px] flex flex-col justify-center items-center lg:justify-normal lg:mx-32 lg:my-0 lg:grid lg:grid-cols-2 lg:py-[72px]"
   >
     <header class="flex justify-center">
       <div>
         <h1
-          class="select-none font-black text-8xl mb-16 lg:mb-0 lg:text-9xl duration-100 transition-colors text-center"
+          class="select-none font-black text-7xl mb-16 lg:mb-0 lg:text-9xl duration-100 transition-colors text-center"
         >
           Let's get in touch!
         </h1>
